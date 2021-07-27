9953f60bb61f38ca757094d9832eb4fc4ce4efd9
Hellisium <maxiima27@gmail.com>
2021-07-27 16:19:07 +0200

    Retweet et like de mots clés Twitter

--- twitterBot/paulsauve.py
+++ twitterBot/paulsauve.py
@@ -2,11 +2,24 @@ import tweepy
 import time
 
 #Accès au compte tweeter
-auth = tweepy.OAuthHandler('KBnjLfAcKbb4t2B5yNabLrUib','4kT5dmBUh6W7kt7VwACT5Q0CFBz9hep28gqtUVVCT9vpG7pFVV')
-auth.set_access_token('1420006259097575425-BIJ2L4tGBY9Z7epooDkPLZTOTD7DwP','Cdb1ot6mDEHP3PHKd6mnujR1Lf2U0uK3dCISdc3trX8vt')
+auth = tweepy.OAuthHandler('9hlV3L2CWhuzBxTMC5iyZuoZ1','e5xbjUCRUHAwdAO2x6sRGXcim73tcwRhMULM3slBLwKhfTOreW')
+auth.set_access_token('1420006259097575425-iaRYNp1JV1HfsrdshvEUnTiHMhv7k5','fnLRpbrMSOCJYgDLkgi1BKoI1H7Ocoj1qsxuuktqTSitJ')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-print(user.screen_name)
+search = 'Business','Consuting','Stratégie Consulting'
+nbTweets = 500 
+
+for tweet in tweepy.Cursor(api.search, search).items(nbTweets):
+    try:
+        tweet.retweet()
+        print('Tweet Reposted')
+        tweet.favorite()
+        print('Tweet liked')
+        time.sleep(2)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break       
